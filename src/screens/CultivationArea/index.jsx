370817d71a2b7d4edadec602bc208fac19ed583a
Viviane Ferreira <vferreiradeoliveira595@gmail.com>
2022-06-11 19:32:11 -0300

    Correções na FlatList da tela de irrigações

--- src/screens/CultivationArea/index.jsx
+++ src/screens/CultivationArea/index.jsx
@@ -4,7 +4,7 @@ import Button from '~/components/global/Button';
 import Input from '~/components/global/Input';
 import InputForm from '~/components/global/InputForm';
 import Layout from '~/components/Layout/index';
-import { TitleScreens } from '~/components/TitleScreens/index';
+import { TitleScreens } from '~/components/TitleScreens';
 import * as Colors from '../../components/UI/variaveis';
 import * as Style from './styles';
 
--- src/screens/NewIrrigation/index.jsx
+++ src/screens/NewIrrigation/index.jsx
@@ -4,14 +4,14 @@ import Button from '~/components/global/Button';
 import Input from '~/components/global/Input';
 import InputForm from '~/components/global/InputForm';
 import Layout from '~/components/Layout';
-import { TitleScreens } from '~/components/TitleScreens/index';
+import { TitleScreens } from '~components/TitleScreens';
 
 import * as Colors from '../../components/UI/variaveis';
 
 export default function NewIrrigation() {
   return (
     <Layout>
-      <TitleScreen style={{ padding: 10 }}>Nova Irrigação</TitleScreen>
+      <TitleScreens style={{ padding: 10 }}>Nova Irrigação</TitleScreens>
     </Layout>
   );
 }
